4b28baa18ad03fed9ad775aba3a2bc57a520c0ce
Son Dang <minhson0506@gmail.com>
2023-04-02 12:18:29 +0300

    finish week 3 assigment

--- src/api/resolvers/catResolver.ts
+++ src/api/resolvers/catResolver.ts
@@ -8,3 +8,51 @@
 // 2.1. createCat
 // 2.2. updateCat
 // 2.3. deleteCat
+
+import {Cat} from '../../interfaces/Cat';
+import {locationInput} from '../../interfaces/Location';
+import rectangleBounds from '../../utils/rectangleBounds';
+import catModel from '../models/catModel';
+
+export default {
+  Query: {
+    cats: async () => {
+      const cats = await catModel.find();
+      return cats;
+    },
+    catById: async (parent: undefined, args: Cat) => {
+      const cat = await catModel.findById(args.id);
+      return cat;
+    },
+    catsByOwner: async (parent: undefined, args: Cat) => {
+      const cats = await catModel.find({owner: args.id});
+      return cats;
+    },
+    catsByArea: async (parent: undefined, args: locationInput) => {
+      const bounds = rectangleBounds(args.topRight, args.bottomLeft);
+      return await catModel.find({
+        location: {
+          $geoWithin: {
+            $geometry: bounds,
+          },
+        },
+      });
+    },
+  },
+  Mutation: {
+    createCat: async (parent: undefined, args: Cat) => {
+      const cat = new catModel(args);
+      return cat.save();
+    },
+    updateCat: async (parent: undefined, args: Cat) => {
+      const cat = await catModel.findByIdAndUpdate(args.id, args, {
+        new: true,
+      });
+      return cat;
+    },
+    deleteCat: async (parent: undefined, args: Cat) => {
+      const cat = await catModel.findByIdAndDelete(args.id);
+      return cat;
+    },
+  },
+};
--- src/api/resolvers/userResolver.ts
+++ src/api/resolvers/userResolver.ts
@@ -6,3 +6,41 @@
 // 2.1. createUser
 // 2.2. updateUser
 // 2.3. deleteUser
+
+import {Cat} from '../../interfaces/Cat';
+import {User} from '../../interfaces/User';
+import userModel from '../models/userModel';
+
+export default {
+  Cat: {
+    owner: async (parent: Cat) => {
+      return await userModel.findById(parent.owner);
+    },
+  },
+  Query: {
+    users: async () => {
+      const users = await userModel.find();
+      return users;
+    },
+    userById: async (parent: undefined, args: User) => {
+      const user = await userModel.findById(args.id);
+      return user;
+    },
+  },
+  Mutation: {
+    createUser: async (parent: undefined, args: User) => {
+      const user = new userModel(args);
+      return user.save();
+    },
+    updateUser: async (parent: undefined, args: User) => {
+      const user = await userModel.findByIdAndUpdate(args.id, args, {
+        new: true,
+      });
+      return user;
+    },
+    deleteUser: async (parent: undefined, args: User) => {
+      const user = await userModel.findByIdAndDelete(args.id);
+      return user;
+    },
+  },
+};
